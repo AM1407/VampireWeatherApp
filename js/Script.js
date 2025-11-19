cb071126089110613a13904e7999015510a6e46f
AMicciche <alessiomicciche@outlook.com>
2025-11-19 16:42:36 +0100

    Enhance weather data display: add units to temperature, humidity, and wind speed

--- js/Script.js
+++ js/Script.js
@@ -1,14 +1,33 @@
-// 1. SETUP: Listen for the "Enter" key on the search bar
+/**
+ * @typedef {Object} WeatherData
+ * @property {string} name
+ * @property {Object} main
+ * @property {number} main.temp
+ * @property {number} main.humidity
+ * @property {number} main.feels_like
+ * @property {Object} wind
+ * @property {number} wind.speed
+ * @property {number} wind.deg
+ * @property {Array} weather
+ * @property {string} weather.0.description
+ * @property {Object} sys
+ * @property {number} sys.sunrise
+ * @property {number} sys.sunset
+ * @property {number} timezone
+ */
+
+
+
 document.getElementById('searchBar').addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
-        getWeather();
+        getWeather().catch(console.error);
     }
 });
 
 async function getWeather() {
 
     const inputElement = document.getElementById('searchBar');
-    const city = inputElement.value; // Grab what the user typed
+    const city = inputElement.value;
 
     const apiKey = 'bac320f5ccfe6ea836c14d2c23cb79f7';
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -23,11 +42,11 @@ async function getWeather() {
 
 
         document.getElementById('placeName').innerText = data.name;
-        document.getElementById('tempBox').innerText = Math.round(data.main.temp);
+        document.getElementById('tempBox').innerText = Math.round(data.main.temp) + "°C";
         document.getElementById('conditionText').innerText = data.weather[0].description;
-        document.getElementById('humidityBox').innerText = data.main.humidity;
-        document.getElementById('windBox').innerText = data.wind.speed;
-        document.getElementById('feelsLike').innerText = Math.round(data.main.feels_like);
+        document.getElementById('humidityBox').innerText = data.main.humidity + "%";
+        document.getElementById('windBox').innerText = data.wind.speed + " m/s";
+        document.getElementById('feelsLike').innerText = Math.round(data.main.feels_like) + "°C";
 
         document.getElementById('timeZone').innerText = new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
 
